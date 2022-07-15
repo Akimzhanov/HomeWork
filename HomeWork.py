b0544e6d5549d5502e44b47acb64cacfd335fe88
Akimzhanov <erasylakimjanov@gmail.com>
2022-07-15 15:11:26 +0600

    homework

---
+++ HomeWork.py
@@ -0,0 +1,84 @@
+from math import pi
+
+# #1
+# r = int(input('Введите число:' ))
+# rp = r + r + r + r
+# rs = r * r
+# print('Периметр квадрата: ', round(rp, 2))
+# print('Площадь квадрата: ', round(rs, 2))
+
+##################################################################
+
+#2
+# rl = int(input('Введите длину: '))
+# rw = int(input('Введите ширину: '))
+# result_l = (rl + rw) * 2
+# result_w = rl * rw
+# print('Периметр прямоугольника: ', result_l)
+# print('Площадь прямоугоника: ',result_w)
+
+# #3
+# r = int(input('Введите радиус: '))
+# result_P = 2 * r * pi
+# result_S = pi * (r ** 2)
+# print('Периметр: ', round(result_P, 1))
+# print('Площадь: ', round(result_S, 1))
+
+# #4
+# num1 = int(input('Введите первое число: '))
+# num2 = int(input('Введите второе число: '))
+# chislo_1 = num1 ** num2
+# chislo_2 = num2 ** num1
+# print('Первое число: ', chislo_1)
+# print('Второе число: ', chislo_2)
+ 
+#5 
+# x = int(input('Введите отрицательное число: '))
+# chislo = abs(x)
+# print(chislo)
+
+#6
+# temperature = float(input('Введите число: '))
+# chislo_T = (temperature - 32) / 1.8
+# print('Температура: ', chislo_T, '°C')
+
+#7
+# a = 3738
+# print('От Бишкека до Москвы ', a * 100, 'метров')
+
+#8
+# ton = 255
+# gram = ton * 1000 * 1000
+# kilogram = ton * 1000
+# centner = ton * 10
+# print(gram, 'грамм')
+# print(kilogram, 'килограмм')
+# print(centner, 'центренов')
+
+#9
+# month = 12 * 7
+# day = 365 * 7
+# print('Месяцев: ', month)
+# print('Дней: ', day)
+
+#10
+# x = 513
+# x1 = 513 / 2.7
+# print('Этажей: ', x1)
+
+#11
+
+mony = 2400
+x = 2400 * 84
+print (x, 'сомов')
+
+
+
+
+
+
+
+
+
+
+
